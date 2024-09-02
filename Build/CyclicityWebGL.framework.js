c8e1a1b04bc340a3f6e0f785adffab9083b9b5e3
RaphAtMatrix <98564085+RaphAtMatrix@users.noreply.github.com>
2024-09-02 12:30:40 +0200

    build

--- Build/CyclicityWebGL.framework.js
+++ Build/CyclicityWebGL.framework.js
@@ -1167,29 +1167,29 @@ var tempDouble;
 var tempI64;
 
 var ASM_CONSTS = {
- 2969692: function() {
+ 2969708: function() {
   Module["emscripten_get_now_backup"] = performance.now;
  },
- 2969747: function($0) {
+ 2969763: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 2969795: function($0) {
+ 2969811: function($0) {
   performance.now = function() {
    return $0;
   };
  },
- 2969843: function() {
+ 2969859: function() {
   performance.now = Module["emscripten_get_now_backup"];
  },
- 2969898: function() {
+ 2969914: function() {
   return Module.webglContextAttributes.premultipliedAlpha;
  },
- 2969959: function() {
+ 2969975: function() {
   return Module.webglContextAttributes.preserveDrawingBuffer;
  },
- 2970023: function() {
+ 2970039: function() {
   return Module.webglContextAttributes.powerPreference;
  }
 };
