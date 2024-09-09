cb85b5b7c0abd7fb1fd5020334719abbd27516a2
RaphAtMatrix <98564085+RaphAtMatrix@users.noreply.github.com>
2024-09-09 15:28:04 +0200

    Build

--- Build/CyclicityWebGL.framework.js
+++ Build/CyclicityWebGL.framework.js
@@ -1311,7 +1311,7 @@ function _BrowserTextUpload(extFilter, gameObjName, dataSinkFn) {
      alert("Error while reading file " + file.name + ": " + loadEvent.target.error);
      return;
     }
-    CyclicityWebGL.SendMessage(inputLoader.gameObjName, inputLoader.dataSinkFn, evt.target.result);
+    HexaGame.SendMessage(inputLoader.gameObjName, inputLoader.dataSinkFn, evt.target.result);
    };
    reader.readAsText(file);
   };
